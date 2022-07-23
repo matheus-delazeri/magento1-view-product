98d2fc8c5048bef7b7472aca9e4c00a97e7ad686
Matheus Delazeri <matheusdelazeris@gmail.com>
2022-07-22 23:06:25 -0300

    Added button to view product in admin product's grid

--- app/code/local/MD/ViewProduct/Helper/Data.php
+++ app/code/local/MD/ViewProduct/Helper/Data.php
@@ -1,4 +1,7 @@
 <?php
 
 class MD_ViewProduct_Helper_Data extends Mage_Core_Helper_Abstract
-{}
+{
+    public $BUTTON_LABEL = "View Product";
+    public $GRID_COLUMN_LABEL = "Open in store";
+}
--- app/code/local/MD/ViewProduct/Model/Observer/Product.php
+++ app/code/local/MD/ViewProduct/Model/Observer/Product.php
@@ -10,7 +10,7 @@ class MD_ViewProduct_Model_Observer_Product
 
         $myButton = $layout->createBlock('adminhtml/widget_button')
             ->setData(array(
-                'label' => Mage::helper('viewproduct')->__('View Product'),
+                'label' => Mage::helper('viewproduct')->BUTTON_LABEL,
                 'onclick' => 'setLocation(\'' . $this->getButtonUrl() . '\')',
                 'class' => 'show-hide'
             ));
@@ -25,8 +25,34 @@ class MD_ViewProduct_Model_Observer_Product
     public function getButtonUrl()
     {
         return Mage::getModel('adminhtml/url')->getUrl('*/view/product', array(
-            '_current'   => true,
+            '_current' => true,
             'active_tab' => null
         ));
+    }
+
+    public function addColumn(Varien_Event_Observer $observer)
+    {
+        $block = $observer->getBlock();
+        if (!isset($block)) return;
+
+        if ($block->getType() == 'adminhtml/catalog_product_grid') {
+            $block->addColumn('view_product', array(
+                'header' => Mage::helper('viewproduct')->GRID_COLUMN_LABEL,
+                'type'   => 'action',
+                'width'  => '70px',
+                'getter' => 'getId',
+                'actions'   => [
+                    [
+                        'caption' => Mage::helper('viewproduct')->BUTTON_LABEL,
+                        'url' => [
+                            'base' => '*/view/product',
+                        ],
+                        'field' => 'id'
+                    ]
+                ],
+                'filter'    => false,
+                'sortable'  => false,
+            ));
+        }
     }
 }
